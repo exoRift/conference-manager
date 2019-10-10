c49bca9ea755734eee38c057c1d5998b83d50fc3
Arthur Neuman <20345400+exoRift@users.noreply.github.com>
2019-10-10 09:00:14 -0400

    Add room selector

---
+++ src/client/Rooms.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {
+  Link
+} from 'react-router-dom'
+
+import './styles/Rooms.css'
+
+const {
+  REACT_APP_API_URL
+} = process.env
+
+class Rooms extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      roomCount: 0
+    }
+  }
+
+  componentDidMount () {
+    fetch(REACT_APP_API_URL + '/roomCount')
+      .then((data) => data.text()
+      .then((roomCount) => this.setState({
+        roomCount
+      })))
+  }
+
+  render () {
+    const roomButtons = []
+
+    for (let i = 1; i <= this.state.roomCount; i++) {
+      roomButtons.push((
+        <Link className='roomButton' to={{
+          pathname: '/room',
+          search: '?room=' + i
+        }} key={i}>
+          <div className='roomNumberContainer'>
+            <font className='roomNumber'>{i}</font>
+          </div>
+        </Link>
+      ))
+    }
+
+    return (
+      <div className='roomContainer'>
+        {roomButtons}
+      </div>
+    )
+  }
+}
+
+export default Rooms
