b780eb16a171d44d7d16d8941ad4041a134e4ae0
Arthur Neuman <20345400+exoRift@users.noreply.github.com>
2020-01-03 12:58:16 -0500

    Fix string types

--- src/server/modules/typeDef.js
+++ src/server/modules/typeDef.js
@@ -1,19 +1,23 @@
+const moment = require('moment')
+
 module.exports = function typeDef (types) {
   return function (req, res, next) {
     for (const type in types) {
-      if (req.body[type] !== undefined) {
-        if (types[type] === 'date') {
-          const result = new Date(req.body[type])
+      if (types[type] === 'date') {
+        const result = new Date(moment(req.body[type])._i)
 
-          if (isNaN(result)) return res.send(400, `invalid param type: ${type}. expected a valid date string`)
-          else req.body[type] = result
-        } else {
-          const receivedType = typeof req.body[type]
+        if (isNaN(result)) return res.send(400, `invalid param type: ${type}. expected a valid date string`)
+        else req.body[type] = result
+      } else {
+        const receivedType = typeof req.body[type]
 
-          if (types[type] === 'array'
-            ? !Array.isArray(req.body[type])
-            : Array.isArray(types[type]) ? !types[type].includes(receivedType) : receivedType !== types[type]) return res.send(400, `invalid param type: ${type}. expected ${types[type]} instead got ${receivedType}`)
-        }
+        if (types[type] === 'array'
+          ? !Array.isArray(req.body[type]) && req.body[type].every((e) => typeof e === 'string')
+          : types[type] === 'number'
+            ? isNaN(Number(req.body[type]))
+            : Array.isArray(types[type])
+              ? !types[type].includes(receivedType)
+              : receivedType !== types[type]) return res.send(400, `invalid param type: ${type === 'array' ? 'array of strings' : type}. expected ${types[type]} instead got ${receivedType}`)
       }
     }
 
