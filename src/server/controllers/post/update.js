fb5a3dd6944ae45a91a0c26a757c37e0bd6c6142
Arthur Neuman <arthurbneuman@gmail.com>
2021-03-24 22:00:16 -0400

    Fixed post update to accommodate incomplete bodies

--- src/server/controllers/post/update.js
+++ src/server/controllers/post/update.js
@@ -1,26 +1,36 @@
 module.exports = {
   requisites: ['authorize', 'argtypes'],
   args: {
-    title: 'string',
-    content: 'string'
+    title: 'opt:string',
+    content: 'opt:string'
   },
   method: 'patch',
   route: '/post/:id/update',
   action: function (req, res) {
     if (req.auth.admin) {
-      return req.db('posts')
-        .update(req.args)
+      return req.db('meetings')
+        .select()
         .where('id', req.params.id)
         .catch((err) => {
           console.error('db', err)
 
           return res.sendError(500, 'internal', 'database unavailable')
         })
-        .then((amount) => {
-          if (amount) {
-            console.log('POST UPDATED:', req.auth.id, req.params.id)
+        .then(([found]) => {
+          if (found) {
+            return req.db('posts')
+              .update(req.args)
+              .where('id', req.params.id)
+              .catch((err) => {
+                console.error('db', err)
 
-            return res.send(200)
+                return res.sendError(500, 'internal', 'database unavailable')
+              })
+              .then(() => {
+                console.log('POST UPDATED:', req.auth.id, req.params.id)
+
+                return res.send(200)
+              })
           } else return res.sendError(404, 'target', 'post does not exist')
         })
     } else return res.sendError(401, 'authorization', 'must be admin')
