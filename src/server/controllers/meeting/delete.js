6cdaaf4241e17e4c90a54af14be2ab683004d302
Arthur Neuman <arthurbneuman@gmail.com>
2021-03-24 22:05:20 -0400

    Improve meeting creator checking

--- src/server/controllers/meeting/delete.js
+++ src/server/controllers/meeting/delete.js
@@ -11,24 +11,24 @@ module.exports = {
 
         return res.sendError(500, 'internal', 'database unavailable')
       })
-      .then(([meeting]) => {
-        if (req.auth.id === meeting.creator) {
-          return req.db('meetings')
-            .delete()
-            .where('id', req.params.id)
-            .catch((err) => {
-              console.error('db', err)
+      .then(([found]) => {
+        if (found) {
+          if (req.auth.id === found.creator) {
+            return req.db('meetings')
+              .delete()
+              .where('id', req.params.id)
+              .catch((err) => {
+                console.error('db', err)
 
-              return res.sendError(500, 'internal', 'database unavailable')
-            })
-            .then((amount) => {
-              if (amount) {
+                return res.sendError(500, 'internal', 'database unavailable')
+              })
+              .then(() => {
                 console.log('MEETING DELETED: ', req.auth.id, req.params.id)
 
                 return res.send(200)
-              }
-            })
-        } else return res.sendError(401, 'authorization', 'you do not own this meeting')
+              })
+          } else return res.sendError(401, 'authorization', 'you do not own this meeting')
+        } else return res.sendError(404, 'target', 'meeting not found')
       })
   }
 }
--- src/server/controllers/meeting/update.js
+++ src/server/controllers/meeting/update.js
@@ -16,50 +16,40 @@ module.exports = {
   route: '/meeting/:id/update',
   action: function (req, res) {
     return req.db('meetings')
-      .select('creator')
+      .select()
       .where('id', req.params.id)
       .catch((err) => {
         console.error('db', err)
 
         return res.sendError(500, 'internal', 'database unavailable')
       })
-      .then(([meeting]) => {
-        if (req.auth.id === meeting.creator) {
-          if (req.args.room <= parseInt(ROOM_COUNT) && req.args.room > 0) {
-            return req.db('meetings')
-              .select()
-              .where('id', req.params.id)
-              .catch((err) => {
-                console.error('db', err)
+      .then(([found]) => {
+        if (found) {
+          if (req.auth.id === found.creator) {
+            if (req.args.room <= parseInt(ROOM_COUNT) && req.args.room > 0) {
+              req.args = {
+                ...found,
+                ...req.args
+              }
 
-                return res.sendError(500, 'internal', 'database unavailable')
-              })
-              .then(([found]) => {
-                if (found) {
-                  req.args = {
-                    ...found,
-                    ...req.args
-                  }
+              return req.util.meeting.validate(req.params.id)
+                .catch((err) => res.sendError(err.code, err.type, err.message))
+                .then(() => req.db
+                  .update(req.args)
+                  .where('id', req.params.id))
+                .catch((err) => {
+                  console.error('db', err)
 
-                  return req.util.meeting.validate(req.params.id)
-                    .catch((err) => res.sendError(err.code, err.type, err.message))
-                    .then(() => req.db
-                      .update(req.args)
-                      .where('id', req.params.id))
-                    .catch((err) => {
-                      console.error('db', err)
+                  return res.sendError(500, 'internal', 'database unavailable')
+                })
+                .then(() => {
+                  console.log('MEETING UPDATED: ', req.auth.id, req.params.id)
 
-                      return res.sendError(500, 'internal', 'database unavailable')
-                    })
-                    .then(() => {
-                      console.log('MEETING UPDATED: ', req.auth.id, req.params.id)
-
-                      return res.send(200)
-                    })
-                } else return res.sendError(404, 'target', 'meeting does not exist')
-              })
-          } else return res.sendError(400, 'argument', `invalid room provided (1-${ROOM_COUNT})`)
-        } else return res.sendError(401, 'authorization', 'you do not own this meeting')
+                  return res.send(200)
+                })
+            } else return res.sendError(400, 'argument', `invalid room provided (1-${ROOM_COUNT})`)
+          } else return res.sendError(401, 'authorization', 'you do not own this meeting')
+        } else return res.sendError(404, 'target', 'meeting does not exist')
       })
   }
 }
