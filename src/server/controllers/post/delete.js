10e5f55138e27b49111b7ca9386aab3153961ad1
Arthur Neuman <arthurbneuman@gmail.com>
2021-03-24 22:09:24 -0400

    Refactor post deletion
    - Fix how post update checks for existence

--- src/server/controllers/post/delete.js
+++ src/server/controllers/post/delete.js
@@ -1,24 +1,20 @@
 module.exports = {
-  requisites: ['authorize', 'argtypes'],
-  args: {
-    title: 'opt:string',
-    content: 'opt:string'
-  },
-  method: 'patch',
-  route: '/post/:id/update',
+  requisites: ['authorize'],
+  method: 'delete',
+  route: '/post/:id/delete',
   action: function (req, res) {
     if (req.auth.admin) {
       return req.db('posts')
-        .update(req.args)
+        .delete()
         .where('id', req.params.id)
         .catch((err) => {
           console.error('db', err)
 
           return res.sendError(500, 'internal', 'database unavailable')
         })
-        .then((updated) => {
-          if (updated.length) {
-            console.log('POST UPDATED:', req.auth.id, req.params.id)
+        .then((amount) => {
+          if (amount) {
+            console.log('POST DELETED:', req.auth.id, req.params.id)
 
             return res.send(200)
           } else return res.sendError(404, 'target', 'post does not exist')
